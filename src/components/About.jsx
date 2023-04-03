84be0fb2284e4f2911ad3ffcafd56a02209b42a6
VladZGarcia <vladi.garcia@gmail.com>
2023-04-03 13:46:20 +0200

    added description and cards

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
 
-const About = () => {
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+        <div options={{
+          max: 45,
+          scale: 1,
+          speed: 450
+        }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 glassName="text-white text-[20px font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        Är en driven 4 barns pappa med mycket energi och tålamod, är som person social och positiv och lättsam att umgås med. Anses vara lugn, speciellt vid stressade situationer och gillar att lösa problem på ett så effektivt sätt som möjligt.
+        Trivs att arbeta självständigt men även i grupper om flera. För mig är kommunikation en viktig del, speciellt vid lösning av problem för att undvika onödig omanalysering/extra arbete/missförstånd gillar att istället försöka ha ett effektivt arbetsflöde.
+        Har erfarenhet i att arbeta under stress och har beskrivits av arbetskamraters som aktiv, noggrann och flexibel.
+
+
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about")
